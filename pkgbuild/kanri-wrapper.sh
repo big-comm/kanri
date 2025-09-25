8329990c4665f585548fddf7e30787b2f15de4bd
talesam <talesam@gmail.com>
2025-09-25 14:27:08 -0300

    🔨 refactor: Refactored PKGBULD and .install

--- pkgbuild/kanri-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# NVIDIA check using lsmod to detect loaded kernel modules
-if lsmod | grep -q '^nvidia '; then
-    export WEBKIT_DISABLE_DMABUF_RENDERER=1
-fi
-
-exec /usr/lib/kanri "$@"
